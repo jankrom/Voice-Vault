f68d323ce5644944d6d1dbdfc714a85f0d6076a7
K3mdi <okemdi.emegwa@gmail.com>
2025-03-24 20:57:17 -0400

    changed activation script

--- device/activate_captive.sh
+++ device/activate_captive.sh
@@ -16,8 +16,17 @@ sudo ip addr add 192.168.4.1/24 dev wlan0
 sudo ip link set wlan0 up
 
 # Start required services
-sudo systemctl start dnsmasq
-sudo systemctl start hostapd
-sudo systemctl start nodogsplash
+# sudo systemctl start dnsmasq
+# sudo systemctl start hostapd
+# sudo systemctl start nodogsplash
+
+sudo systemctl restart dhcpcd
+sudo systemctl restart dnsmasq
+sudo systemctl unmask hostapd
+sudo systemctl enable hostapd
+sudo systemctl restart hostapd
+
+sudo systemctl enable nodogsplash
+sudo systemctl restart nodogsplash
 
 echo "Captive Portal Activated."
