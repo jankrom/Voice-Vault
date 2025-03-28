90bc28853c65e8e8388fd7751dc5cf45703125a6
fullscaleditc <daherman47@gmail.com>
2025-03-28 17:10:36 -0400

    Reapply "fixing captive portal script"
    This reverts commit b958374ccec006a924a4836cc4849875106cf249.

--- device/activate_captive.sh
+++ device/activate_captive.sh
@@ -2,13 +2,13 @@
 
 echo "Activating Captive Portal..."
 
-# Unblock Wi-Fi interface
-sudo rfkill unblock wifi
-sudo rfkill unblock all
-
 # Stop Wi-Fi client (if running)
 sudo systemctl stop wpa_supplicant || true
 sudo nmcli radio wifi off || true
+
+# Unblock Wi-Fi interface
+sudo rfkill unblock wifi
+sudo rfkill unblock all
 
 # Assign static IP
 sudo ip addr flush dev wlan0
@@ -16,15 +16,15 @@ sudo ip addr add 192.168.4.1/24 dev wlan0
 sudo ip link set wlan0 up
 
 # Start required services
-# sudo systemctl start dnsmasq
-# sudo systemctl start hostapd
-# sudo systemctl start nodogsplash
+sudo systemctl start dnsmasq
+sudo systemctl start hostapd
+sudo systemctl start nodogsplash
 
-sudo systemctl restart dhcpcd
-sudo systemctl restart dnsmasq
-sudo systemctl unmask hostapd
-sudo systemctl enable hostapd
-sudo systemctl restart hostapd
+# sudo systemctl restart dhcpcd
+# sudo systemctl restart dnsmasq
+# sudo systemctl unmask hostapd
+# sudo systemctl enable hostapd
+# sudo systemctl restart hostapd
 
 sudo systemctl enable nodogsplash
 sudo systemctl restart nodogsplash
